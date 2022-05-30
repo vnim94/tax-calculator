925f38061da2a14abca567c29739721e2ea31ba9
vnim94 <victor.nim94@outlook.com>
2022-05-30 21:27:17 +1000

    initial commit

---
+++ TaxCalculator.cs
@@ -0,0 +1,9 @@
+public class TaxCalculator
+{
+	public Dictionary<NzTaxRate, decimal> CalculateGST(List<Invoice> orders)
+	{
+		var result = new Dictionary<TaxRate, decimal>();
+		// some logic to calculate NZ GST
+		return result;
+	}
+}
