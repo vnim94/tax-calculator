afe27a0b7550b49b6df4203c7d499ad211070df2
vnim94 <victor.nim@94outlook.com>
2022-06-05 19:02:43 +1000

    updated controller

--- Controller.cs
+++ Controller.cs
@@ -3,19 +3,17 @@ public class Controller
     var calculator;
     var country; // initialised based on user's location
     var orders = new List<Invoice>(); // populate through database query
-    var result;
 
     switch (country)
     {
         case 'NZ':
             calculator = new NzTaxCalculator();
-            result = calculator.CalculateGST(orders);
             break;
         case 'AU':
             calculator = new AuTaxCalculator();
-            result = calculator.CalculateGST(orders);
             break;
     }
 
+    var result = calculator.CalculateGST(orders);
     return result;
 }
