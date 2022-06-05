91b45d7d328a2f467456c9a882c4fda2c2c9cbe9
vnim94 <victor.nim94@outlook.com>
2022-06-05 16:25:16 +1000

    implement strategy pattern in controller

--- Controller.cs
+++ Controller.cs
@@ -1,6 +1,21 @@
 public class Controller
 {
-    var calculator = new TaxCalculator();
-    var result = calculator.CalculateGST(orders);
+    var calculator;
+    var country; // initialised based on user's location
+    var orders = new List<Invoice>(); // populate through database query
+    var result;
+
+    switch (country)
+    {
+        case 'NZ':
+            calculator = new NzTaxCalculator();
+            result = calculator.CalculateGST(orders);
+            break;
+        case 'AU':
+            calculator = new AuTaxCalculator();
+            result = calculator.CalculateGST(orders);
+            break;
+    }
+
     return result;
 }
